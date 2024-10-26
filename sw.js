fe2fd049c22704443d9c1e41d5ef01762b4ff8ab
1123 <1123@student.itgymnazium.cz>
2024-10-26 23:35:33 +0200

    v0.9

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('weather-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/scripts3.js',
+                '/manifest.json',
+                '/NRWW.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
