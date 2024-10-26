68a6921e6fc4d9771e7e03e2ec144a2b60bc8075
1123 <1123@student.itgymnazium.cz>
2024-10-27 00:20:50 +0200

    v0.9.5

--- scripts3.js
+++ scripts3.js
@@ -1,10 +1,15 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
-            .then(registration => console.log('Service Worker zaregistrován s oborem: ', registration.scope))
+            .then(registration => {
+                console.log('Service Worker zaregistrován s oborem: ', registration.scope);
+
+                registration.update();
+            })
             .catch(err => console.log('Registrace Service Worker selhala: ', err));
     });
 }
+
 
 
 
--- sw.js
+++ sw.js
@@ -1,35 +1,24 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        // Nejprve vymaž staré cache
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     return caches.delete(cacheName);
                 })
             );
-        })
-    );
-
-    event.waitUntil(
-        caches.open('weather-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/scripts3.js',
-                '/manifest.json',
-                '/NRWW.png',
-            ]).catch(error => {
-                console.error('Chyba při ukládání do cache:', error);
+        }).then(() => {
+            return caches.open('weather-cache').then(cache => {
+                return cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/style.css',
+                    '/scripts3.js',
+                    '/manifest.json',
+                    '/NRWW.png',
+                ]).catch(error => {
+                    console.error('Chyba při ukládání do cache:', error);
+                });
             });
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
         })
     );
 });
